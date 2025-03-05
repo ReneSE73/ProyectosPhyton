963836b47fe0a1e1bbdf6adb514339d66f71777c
ReneSE73 <renesoes@gmail.com>
2025-03-04 18:58:43 -0600

    primera entrega

---
+++ ProyectIMC.py
@@ -0,0 +1,24 @@
+#Calculo del IMC
+#Autor: René Soriano Estrada
+#Matricula 010705999
+#Universidad UTEL
+
+print("""
+Bienvenido ...
+      El siguiente programa realiza el calculo de Indice de masa corporal (IMC)
+      a partir de que se indique los kilos y la estatura
+
+
+ """)
+
+nombreNombre = input("¿Nombre del paciente?")
+nombreApaterno = input("¿Apellido paterno?")
+nombreAmaterno = input("¿Apellido materno?")
+
+pesoKilos = float(input("Indiquenos el peso en kilos: "))
+estatura = float(input("Indique la estatura e metros: "))
+
+imcResultado = pesoKilos / (estatura**2)
+
+print(nombreNombre + nombreApaterno + nombreAmaterno)
+print(f"su indice de masa corporal es:{imcResultado}" )
